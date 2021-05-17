70878cfbcb8c14ccc76af9e36706d6f2b2046159
Brian Mays <brianmays@me.com>
2021-05-17 13:47:56 -0700

    Updated schema and model to match google API fields.

--- models/book-model.js
+++ models/book-model.js
@@ -3,7 +3,9 @@ const mongoose = require('../db/connection')
 const bookSchema = new mongoose.Schema(
     {
         title: { type: String },
-        author: { type: String },
+        authors: [ { type: String } ],
+        publish_date: { type: String },
+        image_link: { type: String },
         rating: { type: Number },
         review: { type: String }
     }
